5d794c7a53f0482b8f14a4a6691377e4b79e08f7
Leonardo Carvalho Gomes <89273735+leonardoC23111999@users.noreply.github.com>
2021-10-19 13:08:06 -0300

    [Feature Leonardo - Task Blog Pessoal: CREATE Blog Pessoal]

---
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,17 @@
+package com.generation.blogpessoal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/Postagens")
+@CrossOrigin("*")
+public class PostagemController {
+
+		@Autowired
+		private PostagemRepository repository;
+}
